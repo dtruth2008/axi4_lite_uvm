456f30ceed87ed2f70cab3fb92cb3f0bd4be2940
dtruth2008 <darrylm5151@gmail.com>
2025-10-23 22:35:29 -0400

    Update axi5_lite_slave.sv
    Added direction to module ports

--- axi5_lite_slave.sv
+++ axi5_lite_slave.sv
@@ -8,59 +8,69 @@
 //  B2.2 Signal matrix defines ports for the AXI5-Lite Slave
 module axi5_lite_slave(
     // Ports
-
+    
+    // Clock/Reset
+    input ACLK    ,
+    input ARESETn ,
+    
+    // Write address channel
+    input  AWVALID    , 
+    output AWREADY    , 
+    input  AWID       , 
+    input  AWADDR     , 
+    input  AWSIZE     , 
+    input  AWUSER     , 
+    input  AWTRACE    ,  //  Trace signal; implementation define signal for debug
+    input  AWSUBSYSID ,  //  Subsystem identifier that indicates from which subsystem a request originates.
+    input  AWIDUNQ    , 
  
- ACLK- Y Y Y Y Y
- ARESETn- Y Y Y Y Y
- AWVALID- Y Y Y Y Y
- AWREADY AXI_Transport==Ready O Y Y Y Y
-  AWID ID_W_WIDTH>0 YS YS YS YS YS
- AWADDR- Y Y Y Y Y
- AWSIZE SIZE_Present YS YS YS N O
- AWUSER USER_REQ_WIDTH>0 O O O O O
-AWTRACE Trace_Signals O O O O O
- AWSUBSYSID SUBSYSID_WIDTH>0 O O O N O
- AWIDUNQ Unique_ID_Support O O O O O
+    // Write data channel
+    input  WVALID , 
+    output WREADY ,
+    input  WDATA  , 
+    input  WSTRB  , 
+    input  WUSER  , 
+    input  WPOISON,       //  Asserted high to indicate that the data in this transfer is corrupted. There is one bit per 64-bits of data.
+    input  WTRACE , 
+  
+    // Write response channel
+    output BVALID  ,
+    input  BREADY  ,
+    output BID     ,
+    input  BIDUNQ  ,
+    output BRESP   ,
+    output BUSER   ,       // User not recommended due to incompatibility between two compents that define these differently
+    input  BTRACE  ,
  
- WVALID- Y Y Y Y Y
- WREADY AXI_Transport==Ready O Y Y Y Y
- WDATA- Y Y Y Y Y
- WSTRB WSTRB_Present YS YS YS YS YS
-  WUSER USER_DATA_WIDTH>0 O O O O O
- WPOISON Poison O O O O O
- WTRACE Trace_Signals O O O O O
- BVALID- Y Y Y Y Y
- BREADY AXI_Transport==Ready O Y Y Y Y
-  BID ID_W_WIDTH>0 YS YS YS YS YS
- BIDUNQ Unique_ID_Support O O O O O
- BRESP BRESP_WIDTH>0 O O O O O
-  BUSER USER_RESP_WIDTH>0 O O O O O
- BTRACE Trace_Signals O O O O O
- 
-  ARVALID- Y Y Y Y Y
- ARREADY AXI_Transport==Ready O Y Y Y Y
-  ARID ID_R_WIDTH>0 YS YS YS YS YS
-  ARADDR- Y Y Y Y Y
- ARSIZE SIZE_Present YS YS YS N O
+    // Read address channel
+    input  ARVALID ,
+    output ARREADY ,
+    input  ARID    ,
+    input  ARADDR  ,
+    input  ARSIZE  ,
  
  
- ARPROT PROT_Present O YM YM YM YM
-  ARUSER USER_REQ_WIDTH>0 O O O O O
-  ARTRACE Trace_Signals O O O O O
-  ARSUBSYSID SUBSYSID_WIDTH>0 O O O N O
-  ARIDUNQ Unique_ID_Support O O O O O
- 
-  RVALID- Y Y Y Y Y
- RREADY AXI_Transport==Ready O Y Y Y Y
-  RID ID_R_WIDTH>0 YS YS YS YS YS
-   RIDUNQ Unique_ID_Support O O O O O
- RDATA- Y Y Y Y Y
-  RRESP RRESP_WIDTH>0 O O O O O RUSER USER_DATA_WIDTH>0orUSER_RESP_WIDTH>0O O O O O
-  RPOISON Poison O O O O O
-   RTRACE Trace_Signals O O O O O
-   AWAKEUP Wakeup_Signals O O O O O
+    input  ARPROT     ,
+    input  ARUSER     ,
+    input  ARTRACE    ,
+    input  ARSUBSYSID ,
+    input  ARIDUNQ    ,
+    
+    // Read data channel
+    input  RVALID  ,
+    output RREADY  ,
+    input  RID     ,
+    input  RIDUNQ  ,
+    input  RDATA   ,
+    input  RRESP   ,
+    input  RPOISON ,
+    input  RTRACE  ,
+    
    
-   // parity check
+    // Wakeup signals  
+    input  AWAKEUP 
+   
+   // Parity check signals
    
    
   
